813aa6baf0b33b4836fe1b177f786528967edb86
Elias Yona <iconelias001@gmail.com>
2022-12-11 12:20:46 +0300

    Check if an entry if a file or a directory

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CFLAGS=-Wall -Wextra -std=c11 -pedantic -ggdb
 LIBS=
 
-drdupe:
-	$(CC) $(CFLAGS) -o drdupe main.c $(LIBS)
+drdupe: src/main.c
+	$(CC) $(CFLAGS) -o drdupe src/main.c $(LIBS)
--- src/main.c
+++ src/main.c
@@ -1,17 +1,12 @@
+#define _DEFAULT_SOURCE
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 #include <dirent.h>
 
-// TODO: recursively traverse the filesystem
-// TODO: compute the hashes of the files
-// TODO: build the hash table
-int main(int argc, char **argv)
-{
-    (void) argc;
-    (void) argv;
-    const char *dir_path = ".";
+void print_files_recursively(const char *dir_path) {
     DIR *dir =  opendir(dir_path);
 
     if (dir == NULL) {
@@ -23,7 +18,12 @@ int main(int argc, char **argv)
     struct dirent *ent = readdir(dir);
     while (ent != NULL) {
         if (ent->d_name[0] != '.') {
-            printf("File: %s\n", ent->d_name);
+            // check if the entry if a directory or a file
+            if (ent->d_type == DT_DIR) {
+                printf("dir: %s\n", ent->d_name);
+            } else {
+                printf("file: %s\n", ent->d_name);
+            }
         }
         ent = readdir(dir);
     }
@@ -34,5 +34,16 @@ int main(int argc, char **argv)
     }
 
     closedir(dir);
+}
+
+// TODO: recursively traverse the filesystem
+// TODO: compute the hashes of the files
+// TODO: build the hash table
+int main(int argc, char **argv)
+{
+    (void) argc;
+    (void) argv;
+    const char *dir_path = ".";
+    print_files_recursively(dir_path);
     return 0;
 }

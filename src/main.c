ff8d54055371f0c3fc3e671a392a58bc56ccff15
Elias Yona <iconelias001@gmail.com>
2022-12-11 13:05:07 +0300

    Construct relative path

--- src/main.c
+++ src/main.c
@@ -1,12 +1,41 @@
 #define _DEFAULT_SOURCE
 
 #include <stdio.h>
+#include <assert.h>
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 #include <dirent.h>
 
-void print_files_recursively(const char *dir_path) {
+#define PATH_SEP "/"
+#define PATH_SEP_LEN (sizeof(PATH_SEP) - 1)
+
+
+char *join_path(const char *base, const char *file) 
+{
+    size_t base_len = strlen(base);
+    size_t file_len = strlen(file);
+    
+    // beginning
+    char *begin = malloc(base_len + file_len + PATH_SEP_LEN + 1);
+    assert(begin != NULL);
+
+    // assigning end to beginning and append individual parts to end
+    char *end = begin;
+    memcpy(end, base, base_len);
+    end += base_len;
+    memcpy(end, PATH_SEP, PATH_SEP_LEN);
+    end += PATH_SEP_LEN;
+    memcpy(end, file, file_len);
+    end += file_len;
+    *end = '\0';
+
+    return begin;
+}
+
+
+void print_files_recursively(const char *dir_path) 
+{
     DIR *dir =  opendir(dir_path);
 
     if (dir == NULL) {
@@ -17,13 +46,16 @@ void print_files_recursively(const char *dir_path) {
     errno = 0;
     struct dirent *ent = readdir(dir);
     while (ent != NULL) {
-        if (ent->d_name[0] != '.') {
-            // check if the entry if a directory or a file
-            if (ent->d_type == DT_DIR) {
-                printf("dir: %s\n", ent->d_name);
-            } else {
-                printf("file: %s\n", ent->d_name);
+        // check if the entry if a directory or a file
+        // if entry is a directory, loop through it's contents (directories and files)
+        if (ent->d_type == DT_DIR) {
+            if (strcmp(ent->d_name, ".") != 0 && strcmp(ent->d_name, "..") != 0){
+                char *child_path = join_path(dir_path, ent->d_name);
+                print_files_recursively(child_path);
+                free(child_path);
             }
+        } else {
+            printf("file: %s/%s/\n", dir_path, ent->d_name);
         }
         ent = readdir(dir);
     }
@@ -43,7 +75,6 @@ int main(int argc, char **argv)
 {
     (void) argc;
     (void) argv;
-    const char *dir_path = ".";
-    print_files_recursively(dir_path);
+    print_files_recursively(".");
     return 0;
 }
